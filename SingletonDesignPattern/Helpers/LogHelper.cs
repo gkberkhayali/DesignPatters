171c73fb362d973eaf8227b5fdf277b104abbed1
Gökberk Hayali <gokberkhayali@gmail.com>
2022-01-13 12:05:10 +0100

    Initial commit

---
+++ SingletonDesignPattern/Helpers/LogHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.Helpers
+{
+    public class LogHelper
+    {
+        private static LogHelper _logHelper;
+
+        private LogHelper()
+        {
+
+        }
+
+        public static LogHelper GetInstance()
+        {
+            if(_logHelper == null)
+            {
+                _logHelper = new LogHelper();   
+            }
+
+            return _logHelper;
+        }
+
+        public void WriteInformativeLog (string log)
+        {
+            Console.WriteLine(log);
+        }
+
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using SingletonDesignPattern.Helpers;
+
+//Singleton design pattern is based on private constructer logic. There can be only one instance of each class.
+//You can not create object from class but only can access one single instance of it. 
+//For accessing to instance without creating object, we use static keyword. Static keyword is belongs to class itself not any object of it.
+//For example with sample below I will access Print method of LogHelper and use it without creating new object.
+
+LogHelper.GetInstance().WriteInformativeLog("Sample log");
+
+Console.ReadLine();
+
+
+
+
+
+
