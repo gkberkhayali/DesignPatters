31f4603353e8827b376c247bc9338421faf0b1c3
Gökberk Hayali <gokberkhayali@gmail.com>
2022-01-13 12:16:45 +0100

    İnstances hashcode control is implemented.

--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -5,7 +5,28 @@
 //For accessing to instance without creating object, we use static keyword. Static keyword is belongs to class itself not any object of it.
 //For example with sample below I will access Print method of LogHelper and use it without creating new object.
 
+//You see code below will write log to console.
 LogHelper.GetInstance().WriteInformativeLog("Sample log");
+
+
+//Now lets try to create two different objects of loghelper. 
+//These two objects are identical. To check this we can control their hashcodes, values should be same.
+
+var obj1 = LogHelper.GetInstance();
+var obj2 = LogHelper.GetInstance();
+
+
+if (long.Equals(obj1.GetHashCode(),obj2.GetHashCode()))
+{
+    Console.WriteLine("Objects are identical");
+}
+else
+{
+    Console.WriteLine("Objects are different");
+}
+
+
+
 
 Console.ReadLine();
 
